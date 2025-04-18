857638f636894ce7a3fed59e72d8176f00ecd625
Dev3925 <kirtihingrajia@gmail.com>
2025-04-18 20:30:48 +0530

    Add files via upload

---
+++ py ws 1.py
@@ -0,0 +1,206 @@
+#1
+a=5
+b=10
+c=a+b
+print(c)
+'''
+o/p=15
+'''
+
+#2
+a=5
+b=10
+c=b-a
+print(c)
+'''
+o/p =5
+'''
+
+#3
+a=5
+b=5
+c=a*b
+print(c)
+'''
+o/p=25
+'''
+
+
+#4
+a=5
+b=10
+c=b/a
+print(c)
+'''
+o/p=2
+'''
+
+#5
+h=int(input("enter the number :"))
+m=h*60
+print(m)
+'''
+o/p =120
+'''
+
+#6
+m=int(input("enter the number :"))
+h=m/60
+print(h)
+'''
+o/p=2
+'''
+
+#7
+d=int(input("enter the dollars :"))
+rs=d*48
+print(rs)
+'''
+o/p=96
+'''
+
+#8
+rs=int(input("enter the rupess :"))
+d=rs/48
+print(d)
+'''
+o/p=3
+'''
+
+#9
+grms=int(input("enter the grams :"))
+kg=grms/1000
+print(kg)
+'''
+o/p=5
+'''
+
+#10
+kg=int(input("enter the kilograms :"))
+grms=kg*1000
+print(grms)
+'''
+o/p=3
+'''
+
+#11
+bytes=int(input("enter the number of bytes :"))
+KB=bytes/1000
+MB=bytes/1000000
+GB=bytes/1000000000
+print(KB)
+print(MB)
+print(GB)
+'''
+o/p= 1000000
+     1000
+     1
+'''
+
+#12
+celcius=int(input("enter the temperature :"))
+fahrenheit=(9/5*celcius)+32
+print(fahrenheit)
+'''
+o/p=95
+'''
+
+#13
+fahrenheit=int(input("enter the temperature:"))
+celcius=5/9*(fahrenheit-32)
+print(celcius)
+'''
+o/p=16.67
+'''
+
+
+#14
+p=int(input("enter the amount :"))
+r=int(input("enter the rate :"))
+t=int(input("enter the time :"))
+i=prt/100
+print(i)
+'''
+o/p=10
+'''
+
+
+#15
+a=int(input("enter the number :"))
+A=a^2
+P=4*a
+print(A)
+prit(P)
+'''
+o/p=4
+    8
+'''
+
+
+#16
+a=int(input("enter the number :"))
+b=int(input("enter the number :"))
+A=a*b
+P=2(a+b)
+print(A)
+print(P)
+'''
+o/p=50
+    30
+'''
+
+
+#17
+r=int(input("enter the radius :"))
+A=22/7*r*r
+print(A)
+'''
+o/p=154
+'''
+
+
+#18
+h=int(input("enter the height :"))
+l=int(input("enter the length :"))
+A=h*l/2
+print(A)
+'''
+o/p= 25
+'''
+
+#19
+p=int(input("enter the marks :"))
+c=int(input("enter the marks:"))
+m=int(input("enter the marks:"))
+avg=(p+c+m)/3
+print(avg)
+'''
+o/p= 90
+'''
+
+#20
+x=int(input("enter the number :"))
+y=int(input("enter the number :"))
+temp=x
+x=y
+y=temp
+print("value of x is :")
+print("value of y is :")
+'''
+o/p= 5
+     10
+'''
+
+
+#21
+d=int(input("enter dollars:"))
+pounds=d*0.77
+print(pounds)
+'''
+o/p=1.54
+'''
+
+            
+
+
+
